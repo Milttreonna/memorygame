9e927c89ccbc5f653471f834278166334713661f
Nicole Shelton <nshelton@basecampcodingacademy.org>
2016-10-13 15:13:51 -0500

    Changed game around(works)

--- memory.js
+++ memory.js
@@ -1,65 +1,44 @@
-var memory_array = ["yellow", "yellow", "mint", "mint", "pink", "pink"];
-var memory_values = [];
-var memory_tile_ids = [];
-var tiles_flipped = 0;
+var clicks = 0; //counts how may picks have been made in each turn
+var firstchoice; //stores index of first card selected
+var secondchoice; //stores index of second card selected
 
-Array.prototype.memory_tile_shuffle = function() {
-    var i = this.length,
-        j, temp;
-    while (--i > 0) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = this[j];
-        this[j] = this[i];
-        this[i] = temp;
-    }
-}
+var match = 0; //counts matches made
+var backcard = "back.png"; //shows back of card when turned over
 
-function newBoard() {
-    tiles_flipped = 0;
-    var output = '';
-    memory_array.memory_tile_shuffle();
-    for (var i = 0; i < memory_array.length; i++) {
-        output += '<div id="tile_' + i + '" onclick="memoryFlipTile(this,\'' + memory_array[i] + '\')"></div>';
-    }
-    document.getElementById('box').innerHTML = output;
-}
+var faces = []; //array to store card images
+faces[0] = 'mint.png';
+faces[1] = 'yellow.png';
+faces[2] = 'mint.png';
+faces[3] = 'pink.png';
+faces[4] = 'yellow.png';
+faces[5] = 'pink.png';
 
-function memoryFlipTile(tile, val) {
-    if (tile.innerHTML == "" && memory_values.length < 2) {
-        tile.style.background = '#FFF';
-        tile.innerHTML = val;
-        if (memory_values.length == 0) {
-            memory_values.push(val);
-            memory_tile_ids.push(tile.id);
-        } else if (memory_values.length == 1) {
-            memory_values.push(val);
-            memory_tile_ids.push(tile.id);
-            if (memory_values[0] == memory_values[1]) {
-                tiles_flipped += 2;
-                // Clear both arrays
-                memory_values = [];
-                memory_tile_ids = [];
-                // Check to see if the whole board is cleared
-                if (tiles_flipped == memory_array.length) {
-                    alert("Board cleared");
-                    document.getElementById('box').innerHTML = "";
-                    newBoard();
-                }
-            } else {
-                function flip2Back() {
-                    // Flip the 2 tiles back over
-                    var tile_1 = document.getElementById(memory_tile_ids[0]);
-                    var tile_2 = document.getElementById(memory_tile_ids[1]);
-                    tile_1.style.background = 'url(back.png) no-repeat';
-                    tile_1.innerHTML = "";
-                    tile_2.style.background = 'url(back.png) no-repeat';
-                    tile_2.innerHTML = "";
-                    // Clear both arrays
-                    memory_values = [];
-                    memory_tile_ids = [];
-                }
-                setTimeout(flip2Back, 700);
-            }
+function choose(card) {
+        if (clicks == 2) {
+            return;
         }
+        if (clicks == 0) {
+            firstchoice = card;
+            document.images[card].src = faces[card];
+            clicks = 1;
+        } else {
+            clicks = 2;
+            secondchoice = card;
+            document.images[card].src = faces[card];
+            timer = setInterval("check()", 1000);
+        }
+    }
+    /* Check to see if a match is made */
+
+function check() {
+    clearInterval(timer); //stop timer
+    if (faces[secondchoice] == faces[firstchoice]) {
+        match++;
+        document.getElementById("matches").innerHTML = match;
+    } else {
+        document.images[firstchoice].src = backcard;
+        document.images[secondchoice].src = backcard;
+        clicks = 0;
+        return;
     }
 }

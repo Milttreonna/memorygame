50cbd424baa5bfbb0290fc897f317d1996f41359
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-10-14 14:58:24 -0500

    changed check() speed

--- memory.js
+++ memory.js
@@ -40,7 +40,7 @@ function choose(card) {
             clicks = 2;
             secondchoice = card;``
             document.images[card].src = faces[card];
-            timer = setInterval("check()", 500);
+            timer = setInterval("check()", 300);
         }
     }
 
